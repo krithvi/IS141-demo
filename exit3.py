bc4b00e8bf690a018a60092c78763ec16417f0d8
Kavya K Rajendran <52087473+krithvi@users.noreply.github.com>
2024-05-07 18:02:17 -0400

    Exit 3 commit

---
+++ exit3.py
@@ -0,0 +1,13 @@
+score = float(input("Enter your numerical score: "))
+if score < 0 or score > 100:
+	#enter a invalid score message
+#write the condition
+	print("A")
+#score greater than or equal to 80:
+        print("B")
+#score greater than or equal to 70:
+        #what should it print?
+elif score >= 60
+        print("D")
+else: #what should it print?
+	
